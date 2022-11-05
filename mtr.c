29b47ee5d33fa20a17f900b62fe0f90862995bb6
Fernando-Laydner <83661575+Fernando-Laydner@users.noreply.github.com>
2022-11-04 21:28:34 -0300

    Bonito, mas n funciona a fita 5, bento salva ai...

--- mtr.c
+++ mtr.c
@@ -2,13 +2,25 @@
 #include <stdlib.h>
 #include <assert.h>
 
+
+// Opção de debug
 #define DEBUG 1
 
+// Define todas situações da MT
+enum resultado{EXECUTANDO, ACEITO, ERRO, REJEITOU};
+
+// Define qual fita será executada ('5' -> quintupla, '4' -> quadrupla, '69' -> reversível).
+#define FITA 5
+
+// Cria a struc da fita quintupla
 typedef struct transicao_t{
-    char input, output, pos;
-    int estado_input, estado_output;
+    int estado_input;
+    char input;
+    int estado_output;
+    char output, op;
 }transicao_t;
 
+// Cria a struct da fita quadrupla
 typedef struct transicao_q{
     int estado_input;
     char input;
@@ -16,11 +28,13 @@ typedef struct transicao_q{
     char op;
 }transicao_q;
 
+// Cria a struct da MT
 typedef struct mt_t{
     int estado_atual, pos;
     char fita[5000];
 }mt_t;
 
+// Cria a struct das fitas reversíveis, possivelmente não seja necessário
 typedef struct transicao_mf{
     int estado_input;
     char input[3];
@@ -28,8 +42,8 @@ typedef struct transicao_mf{
     char op[3];
 }transicao_mf;
 
-
-int transcao_compara(const void* a, const void* b){
+// Busca o estado correto das fitas quintuplas
+int transicao5_compara(const void* a, const void* b){
     transicao_t *_a = a;
     transicao_t *_b = b;
 
@@ -41,7 +55,8 @@ int transcao_compara(const void* a, const void* b){
     }
 }
 
-int transcao4_compara(const void* a, const void* b){
+// Busca o estado correto das fitas quadruplas
+int transicao4_compara(const void* a, const void* b){
     transicao_q *_a = a;
     transicao_q *_b = b;
 
@@ -53,96 +68,126 @@ int transcao4_compara(const void* a, const void* b){
     }
 }
 
-int transcao_compara_mf(const void* a, const void* b){
+// Talvez não seja necessário, paramos aqui!
+int transcao_mf_compara(const void* a, const void* b){
     transicao_mf *_a = a;
     transicao_mf *_b = b;
 
     if(_a->estado_input == _b->estado_input){
-        return strncmp(_a->input, _b->input, 3);
+        // return strncmp(_a->input, _b->input, 3);
+        return 0;
     }
     else{
         return _a->estado_input - _b->estado_input;
     }
 }
 
-void transicao_imprime(transicao_t *transicao){
+// Imprime as fitas quintuplas
+void transicao5_imprime(transicao_t *transicao){
     #if(DEBUG == 1)
-    printf("(%d,%c)=(%d,%c,%c)\n", transicao->estado_input, transicao->input, transicao->estado_output, transicao->output, transicao->pos);
+    printf("Transicao: (%d,%c)=(%d,%c,%c)\n", transicao->estado_input, transicao->input, transicao->estado_output, transicao->output, transicao->op);
     #endif
 }
 
+// Imprime as fitas quadruplas
 void transicao4_imprime(transicao_q *transicao){
     #if(DEBUG == 1)
-    printf("(%d,%c)=(%d,%c)\n", transicao->estado_input, transicao->input, transicao->estado_output, transicao->op);
+    printf("Transicao: (%d,%c)=(%d,%c)\n", transicao->estado_input, transicao->input, transicao->estado_output, transicao->op);
     #endif
 }
 
-enum resultado{EXECUTANDO, ACEITO, ERRO, REJEITOU};
-
-//type  entrada-quintupla.txt | ./mtr
 int main(void){
-    int n_estado, n_simb, n_trans, n_simb_input;
-    scanf("%d %d %d %d", &n_estado, &n_simb_input, &n_simb, &n_trans);
-    printf("%d %d %d %d\n", n_estado, n_simb_input, n_simb, n_trans);
-    
-    int *estados = malloc(sizeof(int)*n_estado);
 
+// Leitura do arquivo original
+
+    // Primeira linha com número de estados, tamanho do alfabeto da fita, quantidade de estados, número de transições.
+    int n_estado, n_simb, n_trans, n_simb_input;
+
+    scanf("%d %d %d %d", &n_estado, &n_simb_input, &n_simb, &n_trans);
+    printf("Numero de estados: %d\nTamanho do alfabeto da fita: %d\nQuantidade de estados: %d\nNumero de transicoes: %d\n", n_estado, n_simb_input, n_simb, n_trans);
+    
+    // Define os estados da MT
+    int *estados = malloc(sizeof(int)*n_estado);
+    printf("Estados na entrada: [");
     for(int i = 0; i < n_estado; i++){
         scanf("%d", estados+i);
-        printf("%d ", estados[i]);
+        printf("%d", estados[i]);
+        if (i < n_estado-1){printf(", ");}
     }
-    putchar('\n');
+    printf("]\n");
 
+    // Define o alfabeto da Fita
     char *simb_input = malloc(n_simb_input);
-    
+    printf("Alfabeto da fita: [");
     for(int i = 0; i < n_simb_input; i++){
         scanf(" %c", simb_input+i);
-        printf("%c ", simb_input[i]);
+        printf("%c", simb_input[i]);
+        if (i < n_simb_input-1){printf(", ");}
     }
-    putchar('\n');
+    printf("]\n");
 
+    // Define o alfabeto da MT
     char *simb = malloc(n_simb);
-
-    
+    printf("Alfabeto da MT: [");
     for(int i = 0; i < n_simb; i++){
         scanf(" %c", simb+i);
+        printf("%c", simb[i]);
+        if (i < n_simb-1){printf(", ");}
     }
+    printf("]\n");
 
+    // Carrega todas transições do arquivo
     transicao_t *trasicoes = malloc(sizeof(transicao_t)*n_trans);
 
     for(int i = 0; i < n_trans; i++){
-        scanf(" (%d,%c)=(%d,%c,%c)\n", &trasicoes[i].estado_input, &trasicoes[i].input, &trasicoes[i].estado_output, &trasicoes[i].output, &trasicoes[i].pos);
-        transicao_imprime(trasicoes+i);
+        scanf(" (%d,%c)=(%d,%c,%c)\n", &trasicoes[i].estado_input, &trasicoes[i].input, &trasicoes[i].estado_output, &trasicoes[i].output, &trasicoes[i].op);
+        transicao5_imprime(trasicoes+i);
     }
 
+    // Lê a Fita
     mt_t mt = {.estado_atual=1};
-    scanf("%4999s", mt.fita);
-    printf("%s\n\n", mt.fita);
+    scanf(" %4999s", mt.fita);
+    printf("Fita na entrada: %s\n\n", mt.fita);
+
+// Leitura do arquivo original
+
+    // Define a situação da MT
     enum resultado result = EXECUTANDO;
 
-    #if 0
-    qsort(trasicoes, n_trans, sizeof(transicao_t), transcao_compara);
+// Fitas Quintuplas
+    #if(FITA == 5)
+    printf("Inicio das operacoes:\n");
+    // Organiza a fita para poder realizar o bsearch
+    qsort(trasicoes, n_trans, sizeof(transicao_t), transicao5_compara);
     
+    // Realiza as operações.
     for(int i = 0; i < 10000; i++){
+        // Lê o char da fita na posição atual
         char simb_atual = mt.fita[mt.pos];
-        printf("%s", mt.fita);
+        printf("Estado da Fita: %s\n", mt.fita);
+
+        // Impede que seja lido além do ultimo char da fita, talvez não seja necessário...
         if('\0' == simb_atual){simb_atual = simb[n_simb-1];}
 
-        transicao_t chave = {simb_atual, 0 , 0, mt.estado_atual, 0};
-        transicao_t *transicao_atual = bsearch(&chave, trasicoes, n_trans, sizeof(transicao_t), transcao_compara);
+        // Define a chave e realiza a busca na lista de transições
+        transicao_t chave = {mt.estado_atual, simb_atual, 0 , 0, 0};
+        transicao_t *transicao_atual = bsearch(&chave, trasicoes, n_trans, sizeof(transicao_t), transicao5_compara);
 
+        // Se não for encontrado uma transição termina o programa
         if(NULL == transicao_atual){
-            printf("Transicao inexistente");
+            printf("\nTransicao inexistente!\n");
+            // Se DEBUG estiver ativado ela imprime a chave que foi utilizada para causar o erro
+            transicao5_imprime(&chave);
             result = REJEITOU; 
             break;
         }
         else{
-            transicao_imprime(transicao_atual);
+            // Se DEBUG estiver ativado ela imprime a transição que estiver sendo realizada.
+            transicao5_imprime(transicao_atual);
         }
 
-        mt.fita[mt.pos] = transicao_atual->output;
-
-        switch (transicao_atual->pos){
+        // Movimenta o leitor da fita 
+        switch (transicao_atual->op){
         case 'R':
             if(4999 == mt.pos){result = ERRO; exit(EXIT_FAILURE);}
             mt.pos ++; 
@@ -154,33 +199,50 @@ int main(void){
         default: assert(!"Movimento impossivel"); break;
         }
 
+        // Escreve na fita de acordo com a transição atual
+        mt.fita[mt.pos] = transicao_atual->output;
+
+        // Define o novo estado
         mt.estado_atual = transicao_atual->estado_output;
 
+        // Termina o programa se o estado de aceitação (definido como numero de estados da MT original, que é o 6)
+        // Talvez definir o estado de aceitação antes seja uma boa ideia, caso não seja o ultimo.
         if(mt.estado_atual == n_estado){
             result = ACEITO;
             break;
         }
-
     }
-    #elif 1
+
+// Fitas Quadruplas
+    #elif(FITA == 4)
+    // Indica o ultimo estado para adicionar os estados novos e aloca as a quantidade de transições
     int indica_utimo = n_estado+1;
     transicao_q *trasicoes_q = malloc(sizeof(transicao_q)*n_trans*2);
 
+    printf("Novas transicoes:\n");
+    // Converte as transições quintupla em quadruplas 
     for(int i = 0; i < n_trans; i++){
         trasicoes_q[i*2] = (transicao_q){trasicoes[i].estado_input, trasicoes[i].input, indica_utimo, trasicoes[i].output};
-        trasicoes_q[i*2+1] = (transicao_q){indica_utimo, '/', trasicoes[i].estado_output , trasicoes[i].pos};
+        trasicoes_q[i*2+1] = (transicao_q){indica_utimo, '/', trasicoes[i].estado_output , trasicoes[i].op};
         transicao4_imprime(trasicoes_q+i*2);
         transicao4_imprime(trasicoes_q+i*2+1);
         indica_utimo++;
     }
+    printf("\n\nInicio das operacoes:\n");
 
-    qsort(trasicoes_q, n_trans*2, sizeof(transicao_q), transcao4_compara);
+    // Organiza a fita para poder realizar o bsearch
+    qsort(trasicoes_q, n_trans*2, sizeof(transicao_q), transicao4_compara);
     
+    // Realiza as operações.
     for(int i = 0; i < 10000; i++){
+        // Lê o char da fita na posição atual
         char simb_atual = mt.fita[mt.pos];
-        printf("%s", mt.fita);
+        printf("Estado da Fita: %s\n", mt.fita);
+
+        // Impede que seja lido além do ultimo char da fita
         if('\0' == simb_atual){simb_atual = simb[n_simb-1];}
 
+        // Define a chave e realiza a busca na lista de transições
         transicao_q chave;
         if (i%2 == 0){
             chave = (transicao_q){mt.estado_atual, simb_atual, 0 , 0};
@@ -188,57 +250,66 @@ int main(void){
         else{
             chave = (transicao_q){mt.estado_atual, '/', 0 , 0};
         }
+        transicao_q *transicao_atual = bsearch(&chave, trasicoes_q, n_trans*2, sizeof(transicao_q), transicao4_compara);
 
-        transicao_q *transicao_atual = bsearch(&chave, trasicoes_q, n_trans*2, sizeof(transicao_q), transcao4_compara);
-
+        // Se não for encontrado uma transição termina o programa
         if(NULL == transicao_atual){
-            printf("\nTransicao inexistente\n");
+            printf("\nTransição inexistente!\n");
+            // Se DEBUG estiver ativado ela imprime a chave que foi utilizada para causar o erro
             transicao4_imprime(&chave);
             result = REJEITOU; 
             break;
         }
         else{
+            // Se DEBUG estiver ativado ela imprime a transição que estiver sendo realizada.
             transicao4_imprime(transicao_atual);
         }
 
+        // Movimenta o leitor da fita
         if (transicao_atual->input == '/')
             switch (transicao_atual->op){
-            case 'R':
-                if(4999 == mt.pos){result = ERRO; exit(EXIT_FAILURE);}
-                mt.pos ++; 
-                break;
-            case 'L': 
-                if (0 == mt.pos){result = ERRO; exit(EXIT_FAILURE);}
-                mt.pos --; 
-                break;
-            default: assert(!"Movimento impossivel"); break;
+                case 'R':
+                    if(4999 == mt.pos){result = ERRO; exit(EXIT_FAILURE);}
+                    mt.pos ++; 
+                    break;
+                case 'L': 
+                    if (0 == mt.pos){result = ERRO; exit(EXIT_FAILURE);}
+                    mt.pos --; 
+                    break;
+                default: assert(!"Movimento impossivel"); break;
             }
+        // Escreve na fita de acordo com a transição atual e se for diferente de 'B'
         else{
             if (transicao_atual->op != 'B'){
                 mt.fita[mt.pos] = transicao_atual->op;
             }
         }
+
+        // Define o novo estado
         mt.estado_atual = transicao_atual->estado_output;
 
+        // Termina o programa se o estado de aceitação (definido como numero de estados da MT original, que é o 6)
+        // Talvez definir o estado de aceitação antes seja uma boa ideia, caso não seja o ultimo.
         if(mt.estado_atual == n_estado){
             result = ACEITO;
             break;
         }
 
     }
-    #else
+// Fitas Reversíveis WIP
+    #elif(FITA == 69)
     int indica_utimo = n_estado+1;
     transicao_mf *trasicoes_mf = malloc(sizeof(transicao_mf)*n_trans*2);
 
     for(int i = 0; i < n_trans; i++){
         trasicoes_mf[i*2] = (transicao_mf){trasicoes[i].estado_input, trasicoes[i].input, indica_utimo, trasicoes[i].output};
-        trasicoes_mf[i*2+1] = (transicao_mf){indica_utimo, '/', trasicoes[i].estado_output , trasicoes[i].pos};
+        trasicoes_mf[i*2+1] = (transicao_mf){indica_utimo, '/', trasicoes[i].estado_output , trasicoes[i].op};
         transicao4_imprime(trasicoes_mf+i*2);
         transicao4_imprime(trasicoes_mf+i*2+1);
         indica_utimo++;
     }
 
-    qsort(trasicoes_mf, n_trans*2, sizeof(transicao_mf), transcao_compara_mf);
+    qsort(trasicoes_mf, n_trans*2, sizeof(transicao_mf), transcao_mf_compara);
     
     for(int i = 0; i < 10000; i++){
         char simb_atual = mt.fita[mt.pos];
@@ -253,7 +324,7 @@ int main(void){
             chave = (transicao_mf){mt.estado_atual, '/', 0 , 0};
         }
 
-        transicao_q *transicao_atual = bsearch(&chave, trasicoes_mf, n_trans*2, sizeof(transicao_mf), transcao_compara_mf);
+        transicao_q *transicao_atual = bsearch(&chave, trasicoes_mf, n_trans*2, sizeof(transicao_mf), transcao_mf_compara);
 
         if(NULL == transicao_atual){
             printf("\nTransicao inexistente\n");
@@ -292,22 +363,24 @@ int main(void){
     }    
     #endif
 
+    // Escreve a situação da MT
+    printf("\nEstado final da fita: %s\nSituacao da MT: ", mt.fita);
     switch (result){
-    case ACEITO:
-        printf("Aceito");
-        break;
-    case REJEITOU:
-        printf("Rejeitou");
-        break;
-    case ERRO:
-        printf("Erro");
-        break;
-    case EXECUTANDO:
-        printf("Executando");
-        break;
-    default:
-        assert(!"Resultado inesperado");
-        break;
+        case ACEITO:
+            printf("Aceitou");
+            break;
+        case REJEITOU:
+            printf("Rejeitou");
+            break;
+        case ERRO:
+            printf("Erro");
+            break;
+        case EXECUTANDO:
+            printf("Executando");
+            break;
+        default:
+            assert(!"Resultado inesperado");
+            break;
     }
 }
 
